591ff5e63059abbaa70904593ab8c644777a58a6
Morteza Ansarinia <ansarinia@me.com>
2024-12-24 02:00:57 +0100

    re-init using vite

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://behaverse.org/bflow" target="_blank">
+          <img src="logo.png" className="logo" alt="BFlow logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
