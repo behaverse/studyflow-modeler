acb3922bd61a4713fa79e68894889ac2feac25c5
Morteza Ansarinia <ansarinia@me.com>
2024-12-24 02:01:40 +0100

    change title

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
           <img src="logo.png" className="logo" alt="BFlow logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>BFlow Modeler</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
