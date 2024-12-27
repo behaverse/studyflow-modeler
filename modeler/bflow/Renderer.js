372b4a850089a87567b4d6b0894816ead5f04ec7
Morteza Ansarinia <ansarinia@me.com>
2024-12-27 01:52:13 +0100

    render icon on customm bflow elements

--- modeler/bflow/Renderer.js
+++ modeler/bflow/Renderer.js
@@ -1,46 +1,90 @@
 import BaseRenderer from "diagram-js/lib/draw/BaseRenderer";
-import { append as svgAppend, create as svgCreate } from "tiny-svg";
+import {
+  append as svgAppend, create as svgCreate,
+  classes as svgClasses, attr as svgAttr
+} from "tiny-svg";
 
 export default class BFlowRenderer extends BaseRenderer {
 
-  static $inject = ["eventBus", "bpmnRenderer"];
+  static $inject = ["eventBus", "styles", "bpmnRenderer"];
 
-  constructor(eventBus, bpmnRenderer) {
+  constructor(eventBus, styles, bpmnRenderer) {
     const HIGH_PRIORITY = 1500;
     super(eventBus, HIGH_PRIORITY);
+    this.styles = styles;
     this.bpmnRenderer = bpmnRenderer;
+    console.log(this.bpmnRenderer)
   }
 
   canRender(element) {
     return element.type === "bflow:Activity" || element.type === "bflow:RandomAssignment";
   }
 
+  drawDiamond(parentGfx, width, height, attrs) {
+
+    var x_2 = width / 2;
+    var y_2 = height / 2;
+
+    var points = [
+      { x: x_2, y: 0 },
+      { x: width, y: y_2 },
+      { x: x_2, y: height },
+      { x: 0, y: y_2 }
+    ];
+
+    var pointsString = points.map(function(point) {
+      return point.x + ',' + point.y;
+    }).join(' ');
+
+    attrs = this.styles.computeStyle(attrs, {
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeWidth: 2,
+          fill: 'white'
+    });
+
+    var polygon = svgCreate('polygon', {
+      ...attrs,
+      points: pointsString
+    });
+
+    svgAppend(parentGfx, polygon);
+
+    return polygon;
+  }
+
+  drawIcon(parentNode, element, icon, x, y) {
+    var text = svgCreate('text', {
+      x: x,
+      y: y,
+      width: element.width,
+      height: element.height,
+      fontSize: 24,
+      fill: 'purple',
+      class: 'bi'
+    });
+    text.textContent = icon
+    svgAppend(parentNode, text);
+
+    return icon;
+  }
+
   drawShape(parentNode, element) {
     if (element.type === "bflow:Activity") {
-      const rect = svgCreate("rect");
-
-      rect.setAttribute("width", element.width);
-      rect.setAttribute("height", element.height);
-      rect.setAttribute("stroke", "black");
-      rect.setAttribute("fill", "lightgray");
-      rect.setAttribute("stroke-width", 1);
-
-      svgAppend(parentNode, rect);
-
-      return rect;
+      const el = this.bpmnRenderer.handlers["bpmn:Task"](parentNode, element);
+      this.drawIcon(parentNode, element, "\uF502", 5, 29);
+      return el;
     }
 
     if (element.type === "bflow:RandomAssignment") {
-      const polygon = svgCreate("polygon");
-
-      polygon.setAttribute("points", "0,0 20,0 10,20");
-      polygon.setAttribute("stroke", "black");
-      polygon.setAttribute("fill", "lightgray");
-      polygon.setAttribute("stroke-width", 1);
-
-      svgAppend(parentNode, polygon);
-
-      return polygon;
+      var diamond = this.drawDiamond(
+        parentNode, element.width, element.height, {
+          stroke: 'black'
+      });
+      this.drawIcon(parentNode, element, "\uF2FD",
+        element.width / 2 - 12,
+        element.height / 2 + 12);
+      return diamond;
     }
   }
 
