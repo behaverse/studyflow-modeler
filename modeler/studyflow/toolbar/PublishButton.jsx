db9d3d50b6c0c2fe58b886391f3412400f3197b0
Morteza Ansarinia <ansarinia@me.com>
2024-12-31 08:56:52 +0100

    fix xml parsing in the browser

--- modeler/studyflow/toolbar/PublishButton.jsx
+++ modeler/studyflow/toolbar/PublishButton.jsx
@@ -2,7 +2,7 @@ import { useEffect, useContext } from "react";
 
 import {ModelerContext} from '../ModelerContext';
 
-import xml2js from 'xml2js';
+import xmldom from 'xmldom';
 
 export default function ExportButton() {
 
@@ -11,12 +11,26 @@ export default function ExportButton() {
   useEffect(() => {
   }, [modeler]);
 
+  function xml2json(xml, {ignoreTags = []} = {}) {
+    var el = xml.nodeType === 9 ? xml.documentElement : xml
+    if (ignoreTags.includes(el.nodeName)) return el
+  
+    var h  = {_name: el.nodeName}
+    h.content    = Array.from(el.childNodes || []).filter(n => n.nodeType === 3).map(n => n.textContent.trim()).join('')
+    h.attributes = Array.from(el.attributes || []).filter(a => a).reduce((h, a) => { h[a.name] = a.value; return h }, {})
+    h.children   = Array.from(el.childNodes || []).filter(n => n.nodeType === 1).map(c => {
+      var r = xml2json(c, {ignoreTags: ignoreTags})
+      h[c.nodeName] = h[c.nodeName] || r
+      return r
+    })
+    return h
+  }
+
   function publishDiagram() {
     modeler.saveXML({ format: true }).then(({ xml }) => {
-      var parseString = xml2js.parseString;
-      parseString(xml, function (err, result) {
-        console.log(result);
-      });
+      const parsedXml = new xmldom.DOMParser().parseFromString(xml, 'text/xml')
+      const diagramJson = xml2json(parsedXml);
+      console.log(diagramJson);
       alert('Diagram published!');
     });
   }
