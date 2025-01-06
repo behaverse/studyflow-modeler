a326090c5ec98731e7944c56f2c58b65c402442f
Morteza Ansarinia <ansarinia@me.com>
2025-01-06 17:05:48 +0100

    improve hommepage visuals

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import tailwindcss from 'tailwindcss'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '',  // relative base
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    }
+  },
   build: {
     rollupOptions: {
       input: {
