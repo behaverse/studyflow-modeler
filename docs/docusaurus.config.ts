ca34ed265025571aad8cdc993b1613f840061e4c
Morteza Ansarinia <ansarinia@me.com>
2025-12-05 16:55:08 +0100

    fix docs baseUrl and simplify deploy workflow

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -17,10 +17,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://behaverse.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/studyflow-modeler/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
