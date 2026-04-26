7a7cd2831f55b1927d5c0ea6e55fdca6e9b3ba76
Morteza Ansarinia <ansarinia@me.com>
2026-04-26 08:40:52 +0200

    collapse navbar on mobile

--- src/v1/navbar/MenuBar.tsx
+++ src/v1/navbar/MenuBar.tsx
@@ -8,7 +8,7 @@ import { SimulateButton } from './actions/Simulate';
 import { NewDiagramButton } from './actions/NewDiagram';
 import { ResetZoomButton } from './actions/ResetZoom';
 import { LoginButton, LoginDialog } from './actions/Login';
-import { dropdownCls, itemCls, navBtnCls, sepCls } from './styles';
+import { dropdownCls, itemCls, navBtnCls, sepCls, navBurgerBtnCls } from './styles';
 
 export default function MenuBar() {
   const publishDialogRef = useRef<{ open: () => void }>(null);
@@ -19,9 +19,55 @@ export default function MenuBar() {
       <PublishDialog ref={publishDialogRef} />
       <LoginDialog ref={loginDialogRef} />
 
-      <Menu as="div" title="FileMenu" className="">
-        <MenuButton className={navBtnCls}>File</MenuButton>
-        <MenuItems unmount={false} anchor="bottom start" className={dropdownCls}>
+      {/* Desktop */}
+      <div className="hidden sm:flex items-center gap-0.5">
+        <Menu as="div" title="FileMenu">
+          <MenuButton className={navBtnCls}>File</MenuButton>
+          <MenuItems unmount={false} anchor="bottom start" className={dropdownCls}>
+            <MenuItem className={itemCls} as={NewDiagramButton} />
+            <MenuItem className={itemCls} as={OpenButton} />
+            <MenuItem className={itemCls} as={SaveButton} />
+            <MenuSeparator className={sepCls} />
+            <MenuItem className={itemCls} as={ExportButton} fileType="svg" />
+            <MenuItem className={itemCls} as={ExportButton} fileType="png" />
+            <MenuSeparator className={sepCls} />
+            <MenuItem className={itemCls} as={LoginButton} dialog={loginDialogRef} />
+            <MenuItem className={itemCls} as={PublishButton} dialog={publishDialogRef} />
+          </MenuItems>
+        </Menu>
+
+        <Menu as="div" title="ViewMenu">
+          <MenuButton className={navBtnCls}>View</MenuButton>
+          <MenuItems unmount={false} anchor="bottom start" className={dropdownCls}>
+            <MenuItem className={itemCls} as={ResetZoomButton} />
+          </MenuItems>
+        </Menu>
+
+        <Menu as="div" title="HelpMenu">
+          <MenuButton className={navBtnCls}>Help</MenuButton>
+          <MenuItems anchor="bottom start" className={dropdownCls}>
+            <MenuItem>
+              <a href="./docs" target="_blank" className={`block ${itemCls}`}>Docs</a>
+            </MenuItem>
+            <MenuItem>
+              <a href="https://github.com/behaverse/studyflow-modeler" target="_blank" className={`block ${itemCls}`}>GitHub</a>
+            </MenuItem>
+          </MenuItems>
+        </Menu>
+
+        <div className="w-px h-4 bg-white/15 mx-1" />
+        <Menu as="div" title="SimulateMenu">
+          <SimulateButton className="" />
+        </Menu>
+
+      </div>
+
+      {/* Mobile */}
+      <Menu as="div" title="MobileMenu" className="sm:hidden">
+        <MenuButton className={navBurgerBtnCls} aria-label="Menu">
+          <i className="iconify bi--three-dots-vertical text-lg"></i>
+        </MenuButton>
+        <MenuItems unmount={false} anchor="bottom end" className={dropdownCls}>
           <MenuItem className={itemCls} as={NewDiagramButton} />
           <MenuItem className={itemCls} as={OpenButton} />
           <MenuItem className={itemCls} as={SaveButton} />
@@ -31,19 +77,9 @@ export default function MenuBar() {
           <MenuSeparator className={sepCls} />
           <MenuItem className={itemCls} as={LoginButton} dialog={loginDialogRef} />
           <MenuItem className={itemCls} as={PublishButton} dialog={publishDialogRef} />
-        </MenuItems>
-      </Menu>
-
-      <Menu as="div" title="ViewMenu" className="">
-        <MenuButton className={navBtnCls}>View</MenuButton>
-        <MenuItems unmount={false} anchor="bottom start" className={dropdownCls}>
+          <MenuSeparator className={sepCls} />
           <MenuItem className={itemCls} as={ResetZoomButton} />
-        </MenuItems>
-      </Menu>
-
-      <Menu as="div" title="HelpMenu" className="">
-        <MenuButton className={navBtnCls}>Help</MenuButton>
-        <MenuItems anchor="bottom start" className={dropdownCls}>
+          <MenuSeparator className={sepCls} />
           <MenuItem>
             <a href="./docs" target="_blank" className={`block ${itemCls}`}>Docs</a>
           </MenuItem>
@@ -53,10 +89,6 @@ export default function MenuBar() {
         </MenuItems>
       </Menu>
 
-      <div className="w-px h-4 bg-white/15 mx-1" />
-      <Menu as="div" title="SimulateMenu" className="">
-        <SimulateButton className="" />
-      </Menu>
     </div>
   );
 }
--- src/v1/navbar/styles.ts
+++ src/v1/navbar/styles.ts
@@ -1,12 +1,16 @@
 /** Shared Tailwind class strings for the top navigation bar. */
 
 export const navBtnCls =
-  'text-[13px] font-medium text-stone-300 hover:text-white hover:bg-white/10 rounded-md h-7 px-2.5 transition-colors';
+  'text-sm font-medium text-stone-300 hover:text-white hover:bg-white/10 rounded-md h-7 px-2.5 transition-colors';
+
+
+export const navBurgerBtnCls =
+  'text-md font-medium text-stone-300 hover:text-white hover:bg-white/10 rounded-md h-7 px-1.5 transition-colors py-1.5';
 
 export const dropdownCls =
   'bg-stone-900/92 backdrop-blur-xl border border-white/10 text-stone-200 rounded-xl shadow-lg z-50 py-1 min-w-40';
 
 export const itemCls =
-  'px-3 py-1.5 text-[13px] hover:bg-white/10 rounded-lg mx-1 cursor-pointer transition-colors';
+  'px-3 py-1.5 text-sm hover:bg-white/10 rounded-lg mx-1 cursor-pointer transition-colors';
 
 export const sepCls = 'h-px bg-white/12 my-1 mx-2';
